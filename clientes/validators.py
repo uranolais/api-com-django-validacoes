cbef3e86a9e3774d7549b4964903e597cc60fcaa
Laís Urano <95968249+uranolais@users.noreply.github.com>
2022-11-11 15:43:04 -0300

    First Commit

---
+++ clientes/validators.py
@@ -0,0 +1,11 @@
+def cpf_valido(numero_do_cpf):
+    return len(numero_do_cpf) == 11
+
+def nome_valido(nome):
+    return nome.isalpha()
+
+def rg_valido(rg):
+    return len(rg) == 9
+
+def celular_valido(celular):
+    return len(celular) == 11
