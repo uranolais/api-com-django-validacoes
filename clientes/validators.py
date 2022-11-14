c94d2182108d2a9dab6f276768b5621fb9343ffe
Laís Urano <95968249+uranolais@users.noreply.github.com>
2022-11-13 22:59:48 -0300

    Adicionando arquivos da aula03

--- clientes/validators.py
+++ clientes/validators.py
@@ -1,11 +1,21 @@
+import re
+from validate_docbr import CPF
+
 def cpf_valido(numero_do_cpf):
-    return len(numero_do_cpf) == 11
+    cpf = CPF()
+    return cpf.validate(numero_do_cpf)
 
 def nome_valido(nome):
     return nome.isalpha()
 
 def rg_valido(rg):
     return len(rg) == 9
+
+def celular_valido(celular):
+    """Verifica se o celular é valido nessa função (11 91234-1234)"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'#criando o modelo
+    resposta = re.findall(modelo, celular)
+    return resposta
 
 # def celular_valido(celular):
 #     return len(celular) == 11
